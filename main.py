67e76402bce63ade102a33c49532d464c3cfe87e
weee456 <jhweee456@outlook.com>
2023-09-10 15:06:07 +0900

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-import requests
-from tkinter import Tk
-from bs4 import BeautifulSoup
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineCore import *
+from PyQt5.QtWidgets import *
 
-a = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
-class Client:
-    def __init__(self, url: str):
-        self.url = url
-        
-    
-    def connect_for_get(self, target: str):
-        self.req = requests.get(self.url)
-        return BeautifulSoup.get(self.url, "")
+class Gui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-client = Client("https://roblox.com")
-client.connect_for_get(target="a")
+    def initUI(self):
+        self.show()
--- test.py
+++ test.py
@@ -1,21 +1,56 @@
-import webview
-from multiprocessing import Process, Pipe
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Graph(QtGui,QtWidgets):
+    def __init__(self, data, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self._data = data
+        self.resize(400, 700)
+        self.setWindowTitle('FSPwners')
+        self.setAutoFillBackground(True)
+        self.setBackgroundRole(QtGui.QPalette.Base)
 
-def webview_subprocess(child_pipe):
-    window = webview.create_window('TechNote', 'https://technote.kr')
-    webview.start(cmd_recv, [window, child_pipe], gui='cef', debug=True)
+    def paintEvent(self, event):
+        painter = QtGui.QPainter()
+        painter.begin(self)
 
+        screen_width = self.width()
+        screen_height = self.height()
 
-def cmd_recv(window, child_pipe):
-    while True:
-        cmd = child_pipe.recv()
-        # To Do - cmd handler
+        # highest y = max_data_value * y_stretch
+        y_stretch = 15
+        # gap between lower canvas edge and x axis
+        y_gap = 350
+        # stretch enough to get all data items in
+        x_stretch = 10
+        x_width = 20
+        # gap between left canvas edge and y axis
+        x_gap = 20
 
+        for x, y in enumerate(self._data):
+            # calculate reactangle coordinates (integers) for each bar
+            x0 = x * x_stretch + x * x_width + x_gap
+            y0 = screen_height - (y * y_stretch + y_gap)
+            x1 = x0 + x_width
+            y1 = screen_height - y_gap
+            if y < 0:
+                painter.setBrush(QtCore.Qt.red)
+            else:
+                painter.setBrush(QtCore.Qt.green)
+            painter.drawRect(QtCore.QRectF(
+                QtCore.QPointF(x0, y0), QtCore.QPointF(x1, y1)))
+            print (x0, y0, x1, y1)
+
+            # put the y value above each bar
+            painter.drawText(x0 + 2, y0 - 2, str(y))
+
+        painter.end()
 
 if __name__ == '__main__':
-    parent_pipe, child_pipe = Pipe()
 
-    subprocess_handler = Process(target=webview_subprocess, args=(child_pipe,))
-    subprocess_handler.start()
-    subprocess_handler.join()
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    # data to be graphed
+    data = [-20, 15, 10, 7, 5, -4, 3, 2, 1, 1, 0]
+    window = Graph(data)
+    window.show()
+    sys.exit(app.exec_())
